0e394c119ffc554cb2c04681c2c0598f967c667d
NuvoloniM <nuvoloni.m@gmail.com>
2022-04-20 15:33:16 +0200

    LogoComp

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,37 @@
 <template>
   <div id="app">
-    
+    <header>
+      <LogoComp/>
+    </header>
+    <main>
+      <div class="container">
+        <div class="row"></div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-
+import "bootstrap";
+import LogoComp from "./components/LogoComp.vue";
 
 export default {
   name: 'App',
   components: {
-
+    LogoComp
   }
 }
 </script>
 
 <style lang="scss">
+@import "./assets/style/variables.scss";
+@import "bootstrap/dist/css/bootstrap.min.css";
+header {
+  background: $primary_color;
+}
+main {
+  background: $secondary_color;
+  height: calc( 100vh - 50px);
+}
 
 </style>
---
+++ src/components/LogoComp.vue
@@ -0,0 +1,18 @@
+<template>
+    <img src="../assets/img/logo-small.svg" alt="Spotify Logo">
+</template>
+
+<script>
+export default {
+    name: 'LogoComp',
+}
+</script>
+
+<style lang="scss" scoped>
+    img {
+        height: 50px;
+        padding: 10px;
+        filter: invert(54%) sepia(69%) saturate(517%) hue-rotate(89deg) brightness(107%) contrast(95%);
+        z-index: 1;
+    }
+</style>
